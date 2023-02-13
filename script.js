34abae59c8ddad5690b33568003baa623cb61ace
Javad Delavari <112962060+JavadJava@users.noreply.github.com>
2023-02-13 17:10:55 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
+//recieving data from server
 let response = await fetch('./pokemons.json')
 let data = await response.json();
 let pokeCon = document.querySelector(".pokeCon")
 data.forEach(function(pokemon){
-   let pokeDiv = document.createElement("div")
-       pokeDiv.style.position = "relative"
-       pokeDiv.style.width ="20%" 
-       pokeDiv.style.border = "1px black solid"
-       pokeDiv.classList.add(`${pokemon.type}`)
-   let pokePic = document.createElement("img")
-       pokePic.src = pokemon.image
-       pokePic.style.position = "relative";
-       pokePic.style.width = "100%";
-       pokeDiv.append(pokePic)
-   let pokeName = document.createElement("h4")
-       pokeName.innerHTML = pokemon.name
-       pokeDiv.append(pokeName)
-   let pokeType = document.createElement("h4")
-       pokeType.innerHTML = pokemon.type
-       pokeDiv.append(pokeType);
-   pokeCon.append(pokeDiv)
+  // creating and assigning html elements
+let pokeDiv = document.createElement("div") ;
+  pokeDiv.classList.add(`${pokemon.type}`)
+let pokePic = document.createElement("img");
+  pokePic.src = pokemon.image
+  pokeDiv.append(pokePic)
+let pokeName = document.createElement("h4");
+  pokeName.innerHTML = pokemon.name
+  pokeDiv.append(pokeName)   
+let pokeType = document.createElement("h4");
+  pokeType.innerHTML = `${pokemon.type}`
+  pokeDiv.append(pokeType)
+  //
+// adding created div to the base element
+  pokeCon.append(pokeDiv);
+// pokeCon.append(pokeDiv)
 })
